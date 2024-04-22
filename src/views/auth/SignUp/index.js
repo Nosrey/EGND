c55beaeac230cd5f5333e1e4351b05a3051b211b
Luana andrada <luana@yellowpatito.com>
2024-04-22 16:31:30 -0300

    .

--- src/views/auth/SignUp/index.js
+++ src/views/auth/SignUp/index.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import SignUpForm from './SignUpForm'
+import React, { useEffect } from 'react';
+import SignUpForm from './SignUpForm';
 
 function SignUp() {
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
   return (
     <>
       <div className="mb-8">
-        <h3 className="mb-1 text-[#292929] text-[25px] text-center font-bold">Crear Cuenta</h3>
-        <p className="text-[#292929] text-[16px] text-center font-normal">Ingrese los datos para crear una cuenta</p>
+        <h3 className="mb-1 text-[#292929] text-[25px] text-center font-bold">
+          Crear Cuenta
+        </h3>
+        <p className="text-[#292929] text-[16px] text-center font-normal">
+          Ingrese los datos para crear una cuenta
+        </p>
       </div>
       <SignUpForm disableSubmit={false} />
     </>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
