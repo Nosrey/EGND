c4ac13f07499ab8fc3d0980bccbfd0a108d26a34
Luana andrada <luana@yellowpatito.com>
2024-04-22 16:34:07 -0300

    .

--- src/views/auth/SignIn/index.js
+++ src/views/auth/SignIn/index.js
@@ -1,19 +1,29 @@
 import { useMedia } from 'utils/hooks/useMedia';
-import React from 'react'
-import SignInForm from './SignInForm'
+import React, { useEffect } from 'react';
+import SignInForm from './SignInForm';
 
 function SignIn() {
   const media = useMedia();
-
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
   return (
     <>
       <div className="mb-8">
-        <h3 className="mb-1 text-[#292929] text-[25px] text-center font-bold">Iniciar sesión</h3>
-        <p className={`text-[#292929] text-[16px] text-center font-normal ${media !== "mobile" ? "mb-[20px]": ""}`}>Ingrese sus credenciales para acceder a la plataforma</p>
+        <h3 className="mb-1 text-[#292929] text-[25px] text-center font-bold">
+          Iniciar sesión
+        </h3>
+        <p
+          className={`text-[#292929] text-[16px] text-center font-normal ${
+            media !== 'mobile' ? 'mb-[20px]' : ''
+          }`}
+        >
+          Ingrese sus credenciales para acceder a la plataforma
+        </p>
       </div>
       <SignInForm disableSubmit={false} />
     </>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
